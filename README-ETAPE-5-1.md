48f86cdaa37441fc44ad577da46cf934b0dc9132
Mr-loic-08 <loicmdilanm@gmail.com>
2026-09-25 15:05:34 +0100

    25

---
+++ README-ETAPE-5-1.md
@@ -0,0 +1,1 @@
+Correction du bloc Nos partenaires : ajout des fichiers de logos manquants et dimensions d'affichage renforcées.
--- src/app/pages/home/trust/trust.ts
+++ src/app/pages/home/trust/trust.ts
@@ -6,10 +6,10 @@ type Partner = { name:string; alt:string; href:string; img:string; initials:stri
 export class HomeTrustComponent {
  readonly l=inject(LanguageService); focused=0;
  readonly partners:Partner[]=[
-  {name:'CONTABO',alt:'CONTABO',href:'https://my.contabo.com/account/login',img:'images/logos/contabo.png',initials:'CT',c1:'#0f9b8e',c2:'#0b5f57'},
-  {name:'DESJARDINS',alt:'Desjardins',href:'https://www.desjardins.com/qc/fr.html',img:'images/logos/desjardins.png',initials:'DJ',c1:'#12864a',c2:'#0b5c33'},
+  {name:'CONTABO',alt:'CONTABO',href:'https://my.contabo.com/account/login',img:'images/logos/contabo.svg',initials:'CT',c1:'#0f9b8e',c2:'#0b5f57'},
+  {name:'DESJARDINS',alt:'Desjardins',href:'https://www.desjardins.com/qc/fr.html',img:'images/logos/desjardins.svg',initials:'DJ',c1:'#12864a',c2:'#0b5c33'},
   {name:'CAMCCUL',alt:'CAMCCUL',href:'https://camccul.cm',img:'images/logos/camccul.png',initials:'CC',c1:'#1a4f9c',c2:'#0d2b4a'},
-  {name:'OMOA',alt:'OMOA',href:'https://www.omoa-group.com',img:'images/logos/omoa.png',initials:'OM',c1:'#f2a11e',c2:'#c07407'}
+  {name:'OMOA',alt:'OMOA',href:'https://www.omoa-group.com',img:'images/logos/omoa.svg',initials:'OM',c1:'#f2a11e',c2:'#c07407'}
  ];
  get active(){return this.partners[this.focused] ?? this.partners[0]}
  setFocused(i:number){this.focused=i}
